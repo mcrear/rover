9b6d12d8960d7f2eb5a2cc8a016d3c0932540aee
mcrear <mcrear.master@gmail.com>
2022-03-08 21:57:38 +0300

    Set Rover Position

--- Rover/Rover.Main/Program.cs
+++ Rover/Rover.Main/Program.cs
@@ -4,9 +4,46 @@ namespace Rover.Main
 {
     class Program
     {
+        public static int MaxX { get; set; }
+        public static int MaxY { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Write Max Size");
+            var readMaxValues = Console.ReadLine();
+            string[] maxValueList = readMaxValues.Split(' ');
+            MaxX = Convert.ToInt32(maxValueList[0]);
+            MaxY = Convert.ToInt32(maxValueList[1]);
+
+            Console.WriteLine("Write Rover Position");
+            var readRoverPosition = Console.ReadLine();
+            string[] roverPositionList = readRoverPosition.Split(" ");
+
+            int X = Convert.ToInt32(roverPositionList[0]);
+            int Y = Convert.ToInt32(roverPositionList[1]);
+            Direction dir = Enum.Parse<Direction>(roverPositionList[2]);
+
+            Rover rover = new Rover
+            {
+                Direction = dir,
+                X = X,
+                Y = Y
+            };
         }
+    }
+
+    class Rover
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Direction Direction { get; set; }
+    }
+
+    enum Direction
+    {
+        N,
+        S,
+        E,
+        W
     }
 }
